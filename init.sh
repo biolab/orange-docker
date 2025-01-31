c700318430851d4d3750fc4629024e2916db9fc4
Benjamin Škiljan <benjamin102014@gmail.com>
2025-01-31 21:18:25 +0100

    replaced x11vnc with TigerVNC

--- init.sh
+++ init.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-# start Xvfb
-/usr/bin/Xvfb :0 -screen 0 1920x1080x24 &
-sleep 0.1
-
-# start fluxbox
-fluxbox -display :0 &
-
-# start x11vnc
-x11vnc -display :0 -bg -forever -usepw -localhost -xkb -rfbport 5900 &
+# start TigerVNC server (also creates a virtual display and starts Fluxbox window manager)
+vncserver :0 -geometry 2560x1440 -depth 24 -localhost -nolisten tcp -rfbport 5900
 
 # generate self-signed certificate for SSL/TLS encryption
 openssl req -new -x509 -days 365 -nodes -out self.pem -keyout self.pem -subj "/C=US/ST=/L=/O=/CN="
 
 # start noVNC
-/usr/share/novnc/noVNC-1.5.0/utils/novnc_proxy --vnc localhost:5900 --listen 6080 --web /usr/share/novnc/noVNC-1.5.0 --ssl-only &
+/usr/share/novnc/noVNC-1.5.0/utils/novnc_proxy --vnc localhost:5900 --listen 6080 --web /usr/share/novnc/noVNC-1.5.0 --ssl-only --file-only &
 
 # start orange-canvas, QT_AUTO_SCREEN_SCALE_FACTOR=0 is to prevent crashing when loading add-ons
 QT_AUTO_SCREEN_SCALE_FACTOR=0 orange-canvas --no-splash --no-welcome &
